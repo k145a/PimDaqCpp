5ef09203e24f62f6227f4fa416b29e6d77c8ec63
user1 <user1@debian>
2016-05-20 13:51:50 -0700

    testb6

--- PimDaqCpp/src/ledButton.cpp
+++ PimDaqCpp/src/ledButton.cpp
@@ -13,7 +13,7 @@
 
 #define PRU_NUM	0   // using PRU0 for these examples
 
-int main44 (void)
+int main55 (void)
 {
    if(getuid()!=0){
       printf("You must run this program as root. Exiting.\n");
