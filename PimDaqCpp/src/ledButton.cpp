d7b562c3a8be0e163e1a62a68a1bceeb44140e47
user1 <user1@debian>
2016-05-27 12:11:12 -0700

    05/27/16 added gpio.cpp with link errors

--- PimDaqCpp/src/ledButton.cpp
+++ PimDaqCpp/src/ledButton.cpp
@@ -12,18 +12,27 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <iostream>
+#include "GPIO.h"
 
 #define PRU_NUM	0   // using PRU0 for these examples
 
 /*** test definitions ****/
 #define HWREG(x) (*((volatile unsigned int * ) (x)))
+
+using namespace exploringBB;
+using namespace std;
+
+
 int x1, iret1, iret2;
 
 // function run in thread while waiting for input
 void *threadInputDetect(void *value){
 	 // Wait for event completion from PRU, returns the PRU_EVTOUT_0 number
 	   int n = prussdrv_pru_wait_event (PRU_EVTOUT_0);
+	   prussdrv_pru_disable(PRU_NUM);
+	   prussdrv_exit ();
 	   printf("EBB PRU program completed, event number %d.\n", n);
+	   return 0;
 }
 
 /*******************************************/
@@ -34,8 +43,20 @@ int main (void)
       printf("You must run this program as root. Exiting.\n");
     //  exit(EXIT_FAILURE);
    }
-   pthread_t thread; //handle to thread
-   const char *message1 = "thread 1";
+
+   	 GPIO outGPIO(49), inGPIO(115);
+     inGPIO.setDirection(INPUT);    //button is an input
+     outGPIO.setDirection(OUTPUT);  //LED is an output
+     inGPIO.setEdgeType(RISING);    //wait for rising edge
+     outGPIO.streamOpen();          //fast write, ready file
+     outGPIO.streamWrite(LOW);      //turn the LED off
+     cout << "Press the button:" << endl;
+     inGPIO.waitForEdge();          //will wait forever
+     outGPIO.streamWrite(HIGH);     //button pressed, light LED
+     outGPIO.streamClose();         //close the output stream
+
+     pthread_t thread; //handle to thread
+     const char *message1 = "thread 1";
 
 	//RK set pin direction to output
 	int GPIOPin = 48;
@@ -89,7 +110,7 @@ int main (void)
   	   return 1;
      }
    // Disable PRU and close memory mappings
-   prussdrv_pru_disable(PRU_NUM);
-   prussdrv_exit ();
+   //prussdrv_pru_disable(PRU_NUM);
+  // prussdrv_exit ();
    return EXIT_SUCCESS;
 }
